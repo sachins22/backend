8f54add30142a164a96c73252bfe7214ce2e0a8e
sachins22 <ss22sachin@gmail.com>
2024-10-30 13:34:52 +0530

    ss22

--- src/db/index.js
+++ src/db/index.js
@@ -5,8 +5,8 @@ const MONGO_URL ='mongodb+srv://ss22:ss22@cluster0.bpw16.mongodb.net/loginPage_t
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGO_URL}`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
         });
         console.log(`MongoDB connected! DB HOST: ${MONGO_URL}`);
     } catch (error) {
